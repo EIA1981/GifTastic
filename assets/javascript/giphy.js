f1e667681580697777182f5d96ebaea110a09c5b
eia1981 <eiavalos1981@gmail.com>
2019-03-14 22:43:51 -0700

    updated the js https code for the giphy api

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -39,7 +39,7 @@ $( document ).ready(function() {
     //Function that displays all of the gifs
     function displayGifs(){
         var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=RbF4qS84J2XhtH5YtoFi3Jco4pCpdKG4";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=RbF4qS84J2XhtH5YtoFi3Jco4pCpdKG4";
         console.log(queryURL); 
         $.ajax({
             url: queryURL,
